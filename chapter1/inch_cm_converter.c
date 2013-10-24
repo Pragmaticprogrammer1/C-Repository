02d004281943020ff78c1516140bfa00b5877e7a
AK_47 <akalanka595@Akalanka-Karunarathnes-MacBook-Pro.local>
2013-10-24 21:59:17 +1100

    created the chapter1 folder

---
+++ chapter1/inch_cm_converter.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <stdio.h>
+
+int main(void)
+	{
+		int inches;
+			printf("Enter the inch value?\n");
+			float num1;
+			float num2 = 2.54;
+			scanf("%f", &num1);
+			printf ("It is ");
+			printf("%f cm\n", num1*num2);
+		return 0; 
+	}
